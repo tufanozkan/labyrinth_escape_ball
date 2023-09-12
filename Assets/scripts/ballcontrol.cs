6f4a3fc46f144bc09db54320c113c66d2410b535
tufan özkan <tuf.ozkan@gmail.com>
2023-09-12 17:03:21 +0300

    movement finished

--- Assets/scripts/ballcontrol.cs
+++ Assets/scripts/ballcontrol.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class ballcontrol : MonoBehaviour
 {
+    private Rigidbody rg;
+    public float hiz = 1.5f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rg = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void FixedUpdate()
+    {
+        float yatay = Input.GetAxis("Horizontal");
+        float dikey = Input.GetAxis("Vertical");
+        Vector3 kuvvet = new Vector3(dikey, 0, -yatay);
+        rg.AddForce(kuvvet*hiz);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        string objname = collision.gameObject.name;
+        if (objname.Equals("finish"))
+        {
+            Debug.Log("Game Over!!");
+        }
     }
 }
